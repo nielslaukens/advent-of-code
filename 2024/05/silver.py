a847b513ecedc6c4b24df3ac1f89139f162d74a6
Niels Laukens <niels@dest-unreach.be>
2024-12-05 09:57:52 +0100

    2024-12-05 try 2
    A single page number can occur multiple times

--- 2024/05/silver.py
+++ 2024/05/silver.py
@@ -1,5 +1,4 @@
-page_ordering_rules_before: dict[int, int] = {}
-page_ordering_rules_after: dict[int, int] = {}
+page_ordering_rules: dict[int, set[int]] = {}
 pages_to_produce: list[list[int]] = []
 
 with open("input.txt", "r") as f:
@@ -13,36 +12,33 @@ with open("input.txt", "r") as f:
             before, after = line.split('|')
             before = int(before)
             after = int(after)
-            page_ordering_rules_before[after] = before
-            page_ordering_rules_after[before] = after
+            page_ordering_rules.setdefault(before, set())
+            page_ordering_rules[before].add(after)
         elif state == 'produce':
             pages = [int(page) for page in line.split(',')]
             pages_to_produce.append(pages)
 
-print("before: ", page_ordering_rules_before)
-print("after: ", page_ordering_rules_after)
+print("after rules: ", page_ordering_rules)
 print(pages_to_produce)
-
-# ASSUMPTION: every page in pages_to_reproduce has a rule
-for print_job in pages_to_produce:
-    for page in print_job:
-        if page not in page_ordering_rules_after and page not in page_ordering_rules_before:
-            raise RuntimeError(f"Assumption not valid: {page} has no rule")
 
 sum_middle_pages = 0
 for i, print_job in enumerate(pages_to_produce):
     print(f"checking print job {i+1}: {print_job}")
     all_ok = True
-    for before, after in page_ordering_rules_after.items():
-        try:
-            before_pos = print_job.index(before)
-            after_pos = print_job.index(after)
-            if before_pos > after_pos:
-                all_ok = False
-                break
-        except ValueError:  # before and/or after not in print_job
-            # rule does not apply
-            pass
+    for before, afters in page_ordering_rules.items():
+        for after in afters:
+            try:
+                before_pos = print_job.index(before)
+                after_pos = print_job.index(after)
+                if before_pos > after_pos:
+                    print(f"  {before} at {before_pos} is not before {after} at {after_pos}")
+                    all_ok = False
+                    break
+            except ValueError:  # before and/or after not in print_job
+                # rule does not apply
+                pass
+        if not all_ok:
+            break
 
     if all_ok:
         assert len(print_job) % 2 == 1  # assume odd number of pages
@@ -50,4 +46,3 @@ for i, print_job in enumerate(pages_to_produce):
         sum_middle_pages += middle_page
 
 print(f"Sum middle pages: {sum_middle_pages}")
-# 6050 too high
