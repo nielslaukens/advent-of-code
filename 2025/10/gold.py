832de02dec5bf1ef15851372e6e9ac764ded9f2c
Niels Laukens <niels@dest-unreach.be>
2025-12-10 17:33:42 +0100

    2025 10 part 2, try 5

--- 2025/10/gold.py
+++ 2025/10/gold.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import dataclasses
 import re
+import sys
 import typing
 import time
 from pprint import pprint
@@ -33,52 +34,109 @@ with open("input.txt") as f:
             for pos in switch:
                 n[pos] = 1
             switches[i] = n
+
+        switches = sorted(switches, key=lambda x: sum(x), reverse=True)
         machines.append(Machine(switches, joltage))
 
 
+def splits(total: int, parts: int) -> typing.Generator[tuple[int, ...], None, None]:
+    if parts == 0:
+        return
+    if parts == 1:
+        yield (total,)
+        return
+    for i in range(total, -1, -1):
+        for rest in splits(total - i, parts - 1):
+            yield i, *rest
+
+assert list(splits(5, 0)) == []
+assert list(splits(5, 1)) == [(5,)]
+assert list(splits(5, 2)) == [(5,0), (4,1), (3,2), (2,3), (1,4), (0,5)]
+assert list(splits(2, 3)) == [(2,0,0), (1,1,0), (1,0,1), (0,2,0), (0,1,1), (0,0,2)]
+
+
+def do_digit(digit_num: int, switches: list[numpy.ndarray], total: int) -> typing.Generator[tuple[int, ...], None, None]:
+    switches_for_this_digit = []
+    for i, switch in enumerate(switches):
+        if switch[digit_num]:
+            switches_for_this_digit.append(i)
+    for split in splits(total, len(switches_for_this_digit)):
+        num_pushes = [0] * len(switches)
+        for i, j in enumerate(switches_for_this_digit):
+            num_pushes[j] = split[i]
+        yield tuple(num_pushes)
+
+
 class Node:
-    def __init__(self, options: numpy.ndarray, pushes: numpy.ndarray = None):
+    def __init__(
+            self,
+            joltage: numpy.ndarray,
+            switches: list[numpy.ndarray],
+            pushes: numpy.ndarray = None,
+            digits_to_do: list[int] = None
+    ):
         if pushes is None:
-            pushes = numpy.zeros((options.shape[1],), dtype=int)
-        self.options = options
+            pushes = numpy.zeros((len(switches),), dtype=int)
+        if digits_to_do is None:
+            digits_to_do = list(range(len(joltage)))
+            digits_to_do = sorted(digits_to_do, key=lambda d: joltage[d])
+        self.joltage = joltage
+        self.switches = switches
         self.pushes = pushes
+        self.digits_to_do = digits_to_do
         self._value = None
 
-    def branches(self) -> typing.Generator[Node, None, None]:
-        for i in range(self.options.shape[1]):
-            if i < self.options.shape[1]-1 and self.pushes[i+1] != 0:
-                continue
-            j = numpy.zeros(self.pushes.shape, dtype=int)
-            j[i] = 1
-            pushes = self.pushes + j
-            yield Node(self.options, pushes)
-
     def __str__(self) -> str:
-        return f"{self.pushes} => {self.value()}"
+        return f"{self.digits_to_do} {self.pushes} => {self.joltage - self.value()}"
 
     def value(self) -> numpy.ndarray:
         if self._value is None:
-            self._value = numpy.matmul(self.options, self.pushes)
+            self._value = numpy.matmul(self.pushes, self.switches)
         return self._value
+
+    def branches(self):
+        if len(self.digits_to_do) == 0:
+            return
+
+        digit_to_do = self.digits_to_do[0]
+        v = self.value()
+        remaining = self.joltage[digit_to_do] - v[digit_to_do]
+        for pushes in do_digit(digit_to_do, self.switches, remaining):
+            yield Node(
+                self.joltage,
+                self.switches,
+                self.pushes + pushes,
+                self.digits_to_do[1:],
+            )
 
 
 total_presses = 0
-for machine in machines:
+for i, machine in enumerate(machines):
+    print(f"{i}/{len(machines)}: ")
     pprint(machine)
+
     tree = tools.tree.TraverseTreeDepthFirstPre(
-        Node(numpy.array(sorted(machine.switches, key=lambda e: sum(e), reverse=True)).transpose()),
+        Node(
+            machine.joltage,
+            machine.switches,
+        ),
         lambda n: n.branches(),
     )
+    least = None
     for node in tree:
-        if tree.nodes_visited % 1_000_000 == 0:
-            print(tree.nodes_visited, node)
         v = node.value()
-        if numpy.all(v == machine.joltage):
-            print(f"Found {node.value()} after {tree.nodes_visited} with {sum(node.pushes)} pushes")
-            total_presses += sum(node.pushes)
-            break
         if numpy.any(v > machine.joltage):
             tree.dont_descend_into_current_node()
+            continue
+        #print(node)
+        if numpy.all(v == machine.joltage):
+            p = sum(node.pushes)
+            print(f"Found {p} {node.pushes} after {tree.nodes_visited} tries")
+            if least is None or p < least:
+                least = p
+                break  # TODO: is this valid?
+
+    total_presses += least
 
 print(total_presses)
 print(f"Runtime: {time.time()-start_time:.3f} seconds")
